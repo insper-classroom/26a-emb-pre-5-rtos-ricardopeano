28b06bc621508959f7d00e25dfcf7b1d6f237655
Ricardo Peano <ricardocp8@al.insper.edu.br>
2026-04-14 16:48:30 -0300

    ex1

--- exe1/main.c
+++ exe1/main.c
@@ -6,29 +6,44 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-
 const int LED_PIN_R = 4;
 const int LED_PIN_G = 6;
 
 void led_1_task(void *p) {
-  gpio_init(LED_PIN_R);
-  gpio_set_dir(LED_PIN_R, GPIO_OUT);
+    gpio_init(LED_PIN_R);
+    gpio_set_dir(LED_PIN_R, GPIO_OUT);
 
-  int delay = 250;
-  while (true) {
-    gpio_put(LED_PIN_R, 1);
-    vTaskDelay(pdMS_TO_TICKS(delay));
-    gpio_put(LED_PIN_R, 0);
-    vTaskDelay(pdMS_TO_TICKS(delay));
-  }
+    int delay = 250;
+    while (true) {
+        gpio_put(LED_PIN_R, 1);
+        vTaskDelay(pdMS_TO_TICKS(delay));
+        gpio_put(LED_PIN_R, 0);
+        vTaskDelay(pdMS_TO_TICKS(delay));
+    }
+}
+
+void led_2_task(void *p) {
+    gpio_init(LED_PIN_G);
+    gpio_set_dir(LED_PIN_G, GPIO_OUT);
+
+    int delay = 500;
+    while (true) {
+        gpio_put(LED_PIN_G, 1);
+        vTaskDelay(pdMS_TO_TICKS(delay));
+        gpio_put(LED_PIN_G, 0);
+        vTaskDelay(pdMS_TO_TICKS(delay));
+    }
 }
 
 int main() {
-  stdio_init_all();
-  printf("Start RTOS \n");
-  xTaskCreate(led_1_task, "LED_Task 1", 256, NULL, 1, NULL);
-  vTaskStartScheduler();
+    stdio_init_all();
+    printf("Start RTOS \n");
 
-  while (true)
-    ;
+    xTaskCreate(led_1_task, "LED_Task 1", 256, NULL, 1, NULL);
+    xTaskCreate(led_2_task, "LED_Task 2", 256, NULL, 1, NULL);
+
+    vTaskStartScheduler();
+
+    while (true)
+        ;
 }
